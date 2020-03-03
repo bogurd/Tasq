9b8ad432bc9fede0cc60ee17225b2e2095d89a6a
George Katsikas <gkatsika@auth.gr>
2020-03-03 22:33:10 +0200

    use CGPoint addition to position elements

--- Tasq/DifficultySelectionScene.swift
+++ Tasq/DifficultySelectionScene.swift
@@ -14,19 +14,14 @@ class DifficultySelectionScene: SKScene {
     override func didMove(to view: SKView) {
         let newGameTitle = SKLabelNode(text: "New Game")
         newGameTitle.fontSize = 52
-        newGameTitle.position = centerOfScreen.applying(CGAffineTransform.init(translationX: 0, y: screenHeight*CGFloat(3)/CGFloat(8)))
+        newGameTitle.position = centerOfScreen + (0,screenHeight/4)
         self.addChild(newGameTitle)
         
         for (i, difficulty) in GameDifficulty.allCases.enumerated() {
             let difficultyButton = Button(text: "\(difficulty)".capitalized,
                tapCallback: {return self.createGameWithDifficulty(difficulty: difficulty)}
             )
-            difficultyButton.position = newGameTitle.position.applying(
-                CGAffineTransform.init(
-                    translationX: 0,
-                    y: -0.5*CGFloat(i+2)*screenHeight/CGFloat(GameDifficulty.allCases.count)
-                )
-            )
+            difficultyButton.position = newGameTitle.position  + (0,-0.5*CGFloat(i+1)*screenHeight/CGFloat(GameDifficulty.allCases.count))
             self.addChild(difficultyButton)
         }
     }
