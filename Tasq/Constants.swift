81065c706d6bdf52c4719faea06a91e10f1f7cb3
George Katsikas <gkatsika@auth.gr>
2020-03-03 21:40:43 +0200

    add extensions and cleanup Constants

--- Tasq/Constants.swift
+++ Tasq/Constants.swift
@@ -12,10 +12,17 @@ import SpriteKit
 let debugMode = true
 
 let rightPushTransition = SKTransition.push(with: .left, duration: 0.5)
+let leftPushTransition = SKTransition.push(with: .right, duration: 0.5)
 
-var screenHeight = UIScreen.main.bounds.height
-var screenWidth = UIScreen.main.bounds.width
+var screenSize = UIScreen.main.bounds
+var screenHeight = screenSize.height
+var screenWidth = screenSize.width
+
 var centerOfScreen = CGPoint.init(x: screenWidth/2, y: screenHeight/2)
+var topLeft = CGPoint.init(x: 0, y: screenHeight)
+var topRight = CGPoint.init(x: screenWidth, y: screenHeight)
+var bottomLeft = CGPoint(x: 0, y: 0)
+var bottomRight = CGPoint(x: screenWidth, y: 0)
 
 var boardShapeSize = CGSize(width: screenWidth, height: screenWidth)
 
@@ -25,9 +32,6 @@ var lightOnColor = SKColor.init(named: "lightOnColor") ?? SKColor.white
 var lightOffColor = SKColor.init(named: "lightOffColor") ?? SKColor.black
 
 enum GameDifficulty: Int, CaseIterable {
-//    static var allCases: [GameDifficulty] {
-//        return [.easy, .normal, .hard, .extreme]
-//    }
     case easy = 3
     case normal = 5
     case hard = 7
@@ -35,8 +39,45 @@ enum GameDifficulty: Int, CaseIterable {
     case ultra = 11
 }
 
+var unitVec = { angle in
+    return CGPoint(x: CGFloat(cos(angle as Double)),
+                   y: CGFloat(sin(angle as Double))
+    )
+}
+
 extension CaseIterable where Self: RawRepresentable {
     static var AllValues: [RawValue] {
         return allCases.map { $0.rawValue }
+    }
+}
+
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func *(left: CGFloat, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left*right.x, y: left*right.y)
+}
+
+func +(left: CGPoint, right: (CGFloat,CGFloat)) -> CGPoint {
+    return CGPoint(x: left.x + right.0, y: left.y + right.1)
+}
+
+func *(left: CGFloat, right: (CGFloat,CGFloat)) -> CGPoint {
+    return CGPoint(x: left*right.0, y: left*right.1)
+}
+
+extension CGRect {
+    var topLeft: CGPoint {
+        return CGPoint(x: -0.5*self.width, y: 0.5*self.height)
+    }
+    var topRight: CGPoint {
+        return CGPoint(x: 0.5*self.width, y: 0.5*self.height)
+    }
+    var bottomLeft: CGPoint {
+        return CGPoint(x: -0.5*self.width, y: -0.5*self.height)
+    }
+    var bottomRight: CGPoint {
+        return CGPoint(x: 0.5*self.width, y: -0.5*self.height)
     }
 }
